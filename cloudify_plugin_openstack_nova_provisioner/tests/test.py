c7a8d9c56b59e4f2d6ac79ee8b90aac0597ccedd
Ilya Sher <ilya.sher@coding-knight.com>
2014-02-19 12:50:08 +0200

    add tests

---
+++ cloudify_plugin_openstack_nova_provisioner/tests/test.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# vim: ts=4 sw=4 et
+
+import argparse
+import unittest
+
+from cloudify.context import ContextCapabilities
+from cloudify.mocks import MockCloudifyContext
+
+import cosmo_plugin_openstack_common as os_common
+
+import cloudify_plugin_openstack_nova_provisioner.server as cfy_srv
+import cloudify_plugin_openstack_nova_provisioner.monitor as cfy_srv_mon
+
+tests_config = os_common.TestsConfig().get()
+
+
+# WIP - start
+# Maybe this chunk will not be needed as monitoring
+# will be done differently, probably as a (celery) task
+class MockReporter(object):
+    state = {}
+    def start(self, node_id, _host):
+        self.__class__.state[node_id] = 'started'
+    def stop(self, node_id, _host):
+        self.__class__.state[node_id] = 'stopped'
+    
+def _mock_start_monitor(ctx):
+    reporter = MockReporter(ctx)
+    args = argparse.Namespace(monitor_interval=3,
+                              region_name=None)
+    monitor = cfy_srv_mon.OpenstackStatusMonitor(reporter, args)
+    monitor.start()
+
+cfy_srv.start_monitor = _mock_start_monitor
+# WIP - end
+    
+
+class OpenstackNovaTest(os_common.TestCase):
+
+    def test_server_create_and_delete(self):
+        management_network = self.create_network('mng')
+
+        nova_client = self.get_nova_client()
+        name = self.name_prefix + 'srv_crt_del'
+        ctx = MockCloudifyContext(
+            node_id='__cloudify_id_' + name,
+            properties={
+                'server': {
+                    'name': name,
+                    # 'image': nova_client.images.find(name=tests_config['image_name']).id,
+                    'image_name': tests_config['image_name'],
+                    'flavor': tests_config['flavor_id'],
+                    'key_name': tests_config['key_name'],
+                },
+                'management_network_name': management_network['name']
+            }
+        )
+
+        # Test: create
+        self.assertThereIsNoServer(name=name)
+        cfy_srv.create(ctx)
+        self.assertThereIsOneServer(name=name)
+
+        # WIP # # Test: start
+        # WIP # cfy_srv.start(ctx)
+
+        # WIP # # Test: stop
+        # WIP # cfy_srv.stop(ctx)
+
+        # Test: delete
+        # Usually fails here because it takes some time to delete the server
+        cfy_srv.delete(ctx)
+        self.assertThereIsNoServer(name=name)
+
+    @unittest.skip("Not implemented yet")
+    def test_server_in_networks(self):
+        pass
+
+    def _create_networks(self):
+        NETS = 3
+        networks = []
+        for i in range(0, NETS):
+            network = self.create_network('net_' + str(i))
+            self.create_subnet('subnet_' + str(i), '10.1.' + str(i) + '.0/24')
+            networks.append(network)
+        return networks
+
+    def test_server_with_ports(self):
+        name = self.name_prefix + 'srv_w_ports'
+
+        management_network = self.create_network('mng')
+
+        networks = self._create_networks()
+        ports = [self.create_port('port_' + str(i), n) for i, n in enumerate(networks)]
+
+        related = {}
+        for i, port in enumerate(ports):
+            related['related_port_' + str(i)] = { 'external_id': port['id'] }
+
+        ctx = MockCloudifyContext(
+            node_id='__cloudify_id_' + name,
+            properties={
+                'server': {
+                    'name': name,
+                    # 'image': nova_client.images.find(name=tests_config['image_name']).id,
+                    'image_name': tests_config['image_name'],
+                    'flavor': tests_config['flavor_id'],
+                    'key_name': tests_config['key_name'],
+                },
+                'management_network_name': management_network['name']
+            },
+            related=related
+        )
+        self.assertThereIsNoServer(name=name)
+        cfy_srv.create(ctx)
+        self.assertThereIsOneServer(name=name)
+
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
+    # _mock_start_monitor(object())
+
